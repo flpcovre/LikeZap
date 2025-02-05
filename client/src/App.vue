5fac40a89b58f53570c4a44da59e74c69104a515
Filipe Covre <140737398+flpcovre@users.noreply.github.com>
2025-02-04 22:43:45 -0300

    Feature/up tailwind (#2)
    * set up tailwind
    
    * set up flow bite

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import { RouterView } from 'vue-router';
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeScreen from '@/views/HomeScreen.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeScreen,
     },
   ],
 })
