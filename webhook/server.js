9db35669c6c0aff072194fbb4a6a866ec00ea49e
ffcovre <ffcovre@unesc.br>
2025-04-29 17:52:44 -0300

    Add webhook service for WhatsApp integration
    - Introduced a new service 'webhook' in docker-compose.yml with necessary configurations.
    - Created a Dockerfile for the webhook service to set up a Node.js environment using pnpm.
    - Added package.json and pnpm-lock.yaml for dependency management.
    - Implemented server.js to handle incoming webhook requests and verification.

---
+++ webhook/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(express.json());
+dotenv.config();
+
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, WEBHOOK_PORT } = process.env;
+
+app.post("/webhook", async (req, res) => {
+  // log incoming messages
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+
+  // check if the webhook request contains a message
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+  // check if the incoming message contains text
+  if (message?.type === "text") {
+    // extract the business number to send the reply from it
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+  }
+
+  res.sendStatus(200);
+});
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here.</pre>`);
+});
+
+app.listen(WEBHOOK_PORT, () => {
+  console.log(`Server is listening on port: ${WEBHOOK_PORT}`);
+});
