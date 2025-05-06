c13535db71a9f2813c76131784cb5518eac1c3f6
flpcovre <filipecovre224@gmail.com>
2025-05-05 23:55:00 -0300

    Fix event binding in Conversations and ChatHeader components; refactor SidebarWrapper for mobile responsiveness

--- client/components/layout/web/Wrappers/SidebarWrapper.vue
+++ client/components/layout/web/Wrappers/SidebarWrapper.vue
@@ -1,5 +1,34 @@
 <template>
-    <aside class="w-full md:w-[425px] dark:bg-gray-800 border-r dark:border-gray-700 grid grid-cols-[auto,1fr] transition-all duration-300">
-        <slot/>
-    </aside>
+  <aside
+    class="w-full md:w-[425px] dark:bg-gray-800 border-r dark:border-gray-700 grid grid-cols-[auto,1fr] transition-all duration-300 mobile"
+    ref="aside"
+    >
+    <slot />
+  </aside>
 </template>
+
+<script setup lang="ts">
+
+const aside = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  const checkMobile = () => {
+    if (window.innerWidth <= 768) {
+        aside.value?.classList.add('mobile');
+    } else {
+      aside.value?.classList.remove('mobile');
+    }
+  };
+
+  checkMobile();
+  window.addEventListener('resize', checkMobile);
+})
+</script>
+
+<style scoped>
+@media (max-width: 768px) {
+  .mobile {
+    display: none !important;
+  }
+}
+</style>
--- client/pages/chat/[id]/ChatHeader/ChatHeader.vue
+++ client/pages/chat/[id]/ChatHeader/ChatHeader.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="p-5 border-b dark:border-gray-700">
-        <div class="flex items-center">
-            <Icon ri="arrow-left-s-line" customClass="text-[23px] mr-3 cursor-pointer md:hidden" onclick="openChatMobile()"/>
-
-            <div class="w-9 h-9 rounded-full bg-gray-600 flex items-center justify-center">
-                <Icon ri="user-line" customClass="text-gray-300" />
-            </div>
-
-            <h1 class="ml-3 font-semibold text-xl">John Doe</h1>
-
-            <Actions/>
+      <div class="flex items-center">
+        <Icon
+          ri="arrow-left-s-line"
+          customClass="text-[23px] mr-3 cursor-pointer md:hidden"
+          @click="handleBack"
+        />
+  
+        <div class="w-9 h-9 rounded-full bg-gray-600 flex items-center justify-center">
+          <Icon ri="user-line" customClass="text-gray-300" />
         </div>
+  
+        <h1 class="ml-3 font-semibold text-xl">John Doe</h1>
+  
+        <Actions />
+      </div>
     </div>
 </template>
-
+  
 <script setup lang="ts">
-import Icon from '~/components/ui/Icon.vue';
-import Actions from './Actions.vue';
+import Icon from '~/components/ui/Icon.vue'
+import Actions from './Actions.vue'
+  
+function handleBack() {
+    
+}
 </script>
+  
